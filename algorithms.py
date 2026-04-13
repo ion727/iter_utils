f7247b31b37b54eb4913bea734e20b61378b4515
ion727 <ionizer727@gmail.com>
2026-04-13 11:35:37 +0200

    Corrections & import changes

--- algorithms.py
+++ algorithms.py
@@ -1,8 +1,5 @@
-try:
-    from .utils import clamp
-except ImportError:
-    from utils import clamp
 from inspect import getsource
+
 
 class SearchObject:
     def __init__(self, iterable=None, target=None, found=None, occurences=0, floor=None, ceil=None, sorted=None, reverse=None, key=None):
@@ -28,12 +25,12 @@ SearchObject(iterable={self.iterable},
              key={getsource(self.key)[27:]})"""
 
 
-def is_sorted(self, i=..., *, reverse=None, key=None):
+def is_sorted(iterable, i=..., *, reverse=None, key=None):
     if not callable(key):
-        key = self._key
+        key = iterable._key
     # check surrounding the index (i) to ensure they are still sorted
     if reverse is None:
-         reverse = self.is_reversed
+         reverse = iterable.is_reversed
     reverse = -1 if reverse else 1
     rkey = lambda x: reverse*key(x)
     
@@ -41,14 +38,14 @@ def is_sorted(self, i=..., *, reverse=None, key=None):
         if type(i) is not int:
             raise ValueError(f"Error: argument `i`: expected <int>, got {type(i)}")
         if i < 0:
-            if -i > len(self):
-                raise ValueError(f"Error: cannot accept negative index {i} exceeding list length {len(self)}")
-            i = len(self) + i
+            if i < -len(iterable):
+                raise ValueError(f"Error: cannot accept negative index {i} exceeding list length {len(iterable)}")
+            i = len(iterable) + i
         # key(...)*reverse first applies the key to retrieve the desired item, then flips the equality (or not) based on `reverse`` (-1 or 1)
-        return rkey(self.clamped(i-1)) <= rkey(self[i]) <= rkey(self.clamped(i+1))
+        return rkey(iterable.clamp_index(i-1)) <= rkey(iterable[i]) <= rkey(iterable.clamp_index(i+1))
     else:
-        for index in range(len(self)-1):
-            if rkey(self[index]) > rkey(self[index+1]):
+        for index in range(len(iterable)-1):
+            if rkey(iterable[index]) > rkey(iterable[index+1]):
                 return False
         return True
 
@@ -73,42 +70,42 @@ def merge_sort(iterable, *, reverse=False, key=lambda x:x, visual=False):
 
     return iterable
 
-def binary_search(self, target, *, reverse=None, key=None, override_sorted=False):
+def binary_search(iterable, target, *, reverse=None, key=None, override_sorted=False):
     """
     Searches for a specific item in a sorted list, returning first & last occurence indexes
     OUTPUTS:
     SearchObject -- successful search lead to either a match or a range of indexes
     -1 -- ERROR: list is not sorted. Can be overriden with `override_sorted=True`
     """
-    size = len(self)
+    size = len(iterable)
     if size == 0:
-        return SearchObject(iterable=self, 
+        return SearchObject(iterable=iterable, 
                             target=target, 
                             found=False, 
                             sorted=not override_sorted, 
-                            reverse=self.is_reversed)
+                            reverse=iterable.is_reversed)
     
     found = False
     occurences = 0
 
     # check whether the list is sorted in reverse or not
     if reverse is not None:
-        self.is_reversed = reverse
-    rev = -1 if self.is_reversed else 1
+        iterable.is_reversed = reverse
+    rev = -1 if iterable.is_reversed else 1
 
     # retrieve the sorting key
     if not callable(key):
-        key = self._key
+        key = iterable._key
     rkey = lambda x: rev*key(x)
 
     # return error if list is not sorted
-    if not ((self.track_sorted and self.sorted) or override_sorted==True):
+    if not ((iterable.track_sorted and iterable.sorted) or override_sorted==True):
         return -1
 
     rtarget = target*rev
 
     if size == 1:
-        ritem = rkey(self[0])
+        ritem = rkey(iterable[0])
         if rtarget == ritem:
             lwr_bound = upr_bound = 0
             found = True
@@ -122,31 +119,31 @@ def binary_search(self, target, *, reverse=None, key=None, override_sorted=False
         upr_bound = size-1
         while upr_bound - lwr_bound > 1:
             ptr = (lwr_bound + upr_bound) // 2
-            rval = rkey(self[ptr])
+            rval = rkey(iterable[ptr])
             if rval < rtarget:
                 lwr_bound = ptr
             elif rval > rtarget:
                 upr_bound = ptr
             else:
                 # find the lowest occurrence
-                while ptr-1 >= 0 and self[ptr-1] == self[ptr]:
+                while ptr-1 >= 0 and iterable[ptr-1] == iterable[ptr]:
                     ptr -= 1
                 lwr_bound = ptr
 
                 # find the highest occurrence
-                while ptr+1 < size and self[ptr+1] == self[ptr]:
+                while ptr+1 < size and iterable[ptr+1] == iterable[ptr]:
                     ptr += 1
                 upr_bound = ptr
 
                 found = True
                 occurences = upr_bound - lwr_bound + 1
                 break
-    return SearchObject(iterable=self,
+    return SearchObject(iterable=iterable,
                         target=target, 
                         found=found, 
                         occurences=occurences, 
                         floor=lwr_bound, 
                         ceil=upr_bound, 
                         sorted=not override_sorted, 
-                        reverse=self.is_reversed,
+                        reverse=iterable.is_reversed,
                         key=key)
